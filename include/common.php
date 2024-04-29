fb6333ac31233bf2171c5033785a78795ba70105
dinesh-fw <dinesh.singh@freshworks.com>
2024-04-29 12:31:27 +0530

    Update common.php

--- include/common.php
+++ include/common.php
@@ -1,6 +1,6 @@
 <?php
-#session_start();
-//Disable Error Warning
+ #session_start();
+ //Disable Error Warning
 //error_reporting(E_ALL);
 //ini_set('display_errors', 'On');
 
@@ -8,26 +8,25 @@ date_default_timezone_set("Asia/Calcutta");
 
 
 //including common files
-require_once 'vendor/autoload.php';
-require_once 'connection.php';
-require_once 'query.php';
-require_once 'sub_query.php';
-require_once 'function.php';
-require_once 'pattern_function.php';
+  require_once 'vendor/autoload.php';
+  require_once 'connection.php';
+  require_once 'query.php';
+  require_once 'sub_query.php';
+  require_once 'function.php';
+  require_once 'pattern_function.php';
 
 //Adding smarty template
 require_once 'smarty/libs/Smarty.class.php';
 $smarty = new Smarty();
-$smarty->setCaching(Smarty::CACHING_OFF);
+$smarty->caching = false;
+$smarty->force_compile - false;
 $smarty->template_dir = 'views';
 
 
-define('TOKEN', 'BjT2vhi8C4cpXB0VtjTAuMSrrxxWwD14');
-define('KEY', 'asx9hj1ykmv09kgc');
-define('ALLOCATE_PRICE',25000);
-define('OPTION_MONTH','APR');
-define ('TWILIO_SID','ACfae6d012234d7f7af49afbe88e254447');
-define ('TWILIO_TOKEN','b8ce01a7e52e46d86ef3604128d7f9de');
+define('TOKEN', 'xxx');
+define('KEY', 'xx');
+define('ALLOCATE_PRICE',100000);
+define('OPTION_MONTH','MAR');
 
 
 
