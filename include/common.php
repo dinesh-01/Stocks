c14f980c35fb533466c1c21bb19d009ef9ed6b81
dinesh-fw <dinesh.singh@freshworks.com>
2024-08-17 23:33:52 +0530

    Update common.php

--- include/common.php
+++ include/common.php
@@ -14,19 +14,22 @@ require_once 'query.php';
 require_once 'sub_query.php';
 require_once 'function.php';
 require_once 'pattern_function.php';
+require_once 'kite_function.php';
 
 //Adding smarty template
 require_once 'smarty/libs/Smarty.class.php';
 $smarty = new Smarty();
-$smarty->caching = false;
-$smarty->force_compile - false;
+$smarty->force_compile = true;
 $smarty->template_dir = 'views';
 
 
-define('TOKEN', 'xxx');
-define('KEY', 'xx');
+define('TOKEN', 'x');
+define('KEY', 'x');
 define('ALLOCATE_PRICE',100000);
-define('OPTION_MONTH','MAR');
+define('TIME_FRAME',15);
+
+define('EXPIRY_ALLOCATE_PRICE',10000);
+define('OPTION_MONTH','AUG');
 
 
 
